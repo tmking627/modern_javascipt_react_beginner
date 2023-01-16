0590de6994011f980f93e46cb550dfd7a22fbaaa
tmking627 <tmy6254789@gmail.com>
2023-01-17 00:10:46 +0900

    testコード修正

--- src/TodoArea.test.jsx
+++ src/TodoArea.test.jsx
@@ -1,16 +1,29 @@
-import {render, screen} from '@testing-library/react';
+import {render, screen,fireEvent} from '@testing-library/react';
 import { TodoArea } from './TodoArea';
 
 describe("Todoリスト表示テスト", () => {
     test('inputTodoの表示テスト', () => {
         render(<TodoArea />);
-        const inputTodoButton = screen.getByRole('inputtodo');
-        expect(inputTodoButton).toBeInTheDocument();
+        const inputText = screen.getByRole('inputtodo');
+        expect(inputText).toBeInTheDocument();
     });
 });
 
 describe("Todo追加処理のテスト", () => {
+    test('追加処理', () => {
+        //Arrange
+        render(<TodoArea />);
+        const inputText = screen.getByRole('inputtodo');
+        const inputTodoButton = screen.getByRole('button',{name:'追加'});
 
+        //Act
+        //Todoの文字入力も
+        fireEvent.click(inputTodoButton);
+
+        //Assertion
+        const Todo = screen.getByRole('todo-1');
+        expect(Todo).toBeInTheDocument();
+    })
 });
 
 describe("Todo削除処理のテスト", () => {
