e542d913f7c5e8461a58291470497f785f9a1b07
tmking627 <tmy6254789@gmail.com>
2023-01-28 00:52:46 +0900

    react router 追加

--- src/TodoArea.jsx
+++ src/TodoArea.jsx
@@ -3,6 +3,14 @@ import { InputTodo } from "./components/inputTodo";
 import {Todos} from "./components/Todos";
 import { SearchTodo } from "./components/SearchTodo";
 import { TodoContext } from "./components/provider/TodoProvider";
+import {
+  createBrowserRouter,
+  BrowserRouter,
+  RouterProvider,
+  Route,
+  Routes,
+  Link,
+} from "react-router-dom";
 
 export const TodoArea = (props) => {
     const name = props.name;
@@ -18,24 +26,30 @@ export const TodoArea = (props) => {
     const disabled = todos.length > 10;
 
     return (
-        <>
+        <BrowserRouter>
         {/* <InputTodo />
         <SearchTodo /> */}
-        <InputTodo disabled={disabled} inputText={inputText} onChange={onChangeInputText} onClick={onClickAddTodo} />
-        <SearchTodo onChangeSearchWord={onChangeSearchWord} />
-        {disabled && (
-            <p style={{ color: 'red' }}>todoは10個までです。</p>
-        )
-        }
-        {searchTodos === false  ? (
-            <Todos todos={todos} onClick={onClickDeleteTodo} />
-        ) : (
-        <>
-        <p>searchtodo</p>
-        <Todos todos={searchTodos} onClick={onClickDeleteTodo} />
-        </>
-        )
-        }
-        </>
+        <Routes>
+            <Route path="/create/">
+                <InputTodo disabled={disabled} inputText={inputText} onChange={onChangeInputText} onClick={onClickAddTodo} />
+            </Route>
+            <Route path="/">
+                <SearchTodo onChangeSearchWord={onChangeSearchWord} />
+                {disabled && (
+                    <p style={{ color: 'red' }}>todoは10個までです。</p>
+                )
+                }
+                {searchTodos === false  ? (
+                    <Todos todos={todos} onClick={onClickDeleteTodo} />
+                ) : (
+                <>
+                    <p>searchtodo</p>
+                    <Todos todos={searchTodos} onClick={onClickDeleteTodo} />
+                </>
+                )
+                }
+            </Route>
+        </Routes>
+        </BrowserRouter>
     );
 }
